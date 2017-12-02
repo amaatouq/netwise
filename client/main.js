05429f7fd2988762a9adb859ae42c4c7225c66f7
Abdullah Mac <amaatouq@gmail.com>
2017-12-02 15:56:19 -0500

    added react-dom as a package (was missing and throwing a warning) and added a todo (asking Nicolas: where are we importing main.html? how does the selector 'getElementById' knows which document we are referring to?

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@
 import '/imports/startup/both';
 import '/imports/startup/client';
 
+//todo: ask Nicolas why we are not importing main.html here? how come document.getEelementByID knows which file we are referring to?
+
 import { render } from 'react-dom';
 import { renderRoutes } from '../imports/startup/client/routes';
 
