1c3ff4de6668b9fdd96ffd7294132725b1bc55e5
Nicolas Paton <nicolas.paton@gmail.com>
2017-12-03 02:01:50 +0800

    App setup

---
+++ imports/api/players/players.js
@@ -0,0 +1,16 @@
+import SimpleSchema from "simpl-schema";
+import { TimestampSchema } from "../default-schema";
+
+const avatarValues = [];
+
+export const Players = new MongoCollection("players");
+
+Players.schema = new SimpleSchema({
+  avatar: {
+    type: String,
+    optional: true,
+    allowedValues: avatarValues
+  }
+});
+
+Player.schema.extend(TimestampSchema);
---
+++ imports/startup/client/routes.js
@@ -0,0 +1,12 @@
+import { Route, Router } from "react-router";
+import { createBrowserHistory } from "history";
+import React from "react";
+
+import Home from "../../ui/pages/Home";
+
+const browserHistory = createBrowserHistory();
+export const renderRoutes = () => (
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+  </Router>
+);
