196930675b386d5ed76f5137b8bd75f4ea11a6d8
Catherine Li <catheryn@mit.edu>
2018-02-28 12:55:54 -0500

    Prisoners dilemma setup

--- app/imports/game/client/components/TaskResponse.jsx
+++ app/imports/game/client/components/TaskResponse.jsx
@@ -1,96 +1,77 @@
 import React from "react";
 import { Slider } from "@blueprintjs/core";
 
-//TODO: this is not a good component as is for the following reasons
-//    It should have a 'null' default value while this can't be done with the default HTML <input> .. having a default value would lead to anchoring bias for the participant
-//    once a player chose a value, it should be sticky (i.e., saved and retrieved in the next stages)
-//    we might want to store to seperate values (last value as an answer and also intermediate values)
-// NP: Setting to 0 for now otherwise it will break things to have a submittable
-// null. Eventually we can create our own slider that can handle nil and report
-// and error if someone submits null.
 export default class TaskResponse extends React.Component {
-  handleChange = num => {
+
+  handleSubmit = action => {
+    console.log("handleSubmit called with arg:", action)
+
     const { stage, round } = this.props;
-    if (stage.name !== "outcome") {
-      const value = Math.round(num * 100) / 100;
-      stage.set("guess", value);
-      round.set("guess", value);
-    }
+    stage.set("action", action);
+    round.set("action", action);
+    stage.submit();
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.stage.submit();
-  };
+  renderStageFinished() {
+    return (
+      <div className="task-response">
+        <div className="pt-callout .modifier">
+          <h5>Waiting on other players...</h5>
+          Please wait until all players are ready
+        </div>
+      </div>
+    );
+  }
+
+  renderResult(round) {
+    return (
+      <table className="pt-table  pt-html-table pt-html-table-bordered">
+        <thead>
+          <tr>
+            <th>Your action</th>
+            <th>Your partner's action</th>
+            <th>Your payout</th>
+            <th>Your partner's payout</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{ round.get("action") }</td>
+            <td>??</td>
+            <td>??</td>
+            <td>??</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+
+  renderActions() {
+    return (
+      <div className="task-actions">
+        <button onClick={() => this.handleSubmit('cooperate')}>Cooperate</button>
+        <button onClick={() => this.handleSubmit('compete')}>Compete</button>
+      </div>
+    );
+  }
 
   render() {
     const { stage, round } = this.props;
-
-    if (stage.finished) {
-      return (
-        <div className="task-response">
-          <div className="pt-callout .modifier">
-            <h5>Waiting on other players...</h5>
-            Please wait until all players are ready
-          </div>
-        </div>
-      );
-    }
-
+    console.log("stage", stage.get("action"));
+    console.log("round", round.get("action"));
     const isResult = stage.name === "outcome";
+
+    { stage.finished ? this.renderStageFinished() : null }
+
     return (
       <div className="task-response">
-        <form onSubmit={this.handleSubmit}>
-          <div className="pt-form-group">
-            {isResult ? (
-              ""
-            ) : (
-              <label className="pt-label">
-                Your current guess of the correlation is: {round.get("guess")}
-              </label>
-            )}
+        { isResult ? this.renderResult(round) : this.renderActions() }
 
-            <div className="pt-form-content">
-              <Slider
-                min={0}
-                max={1}
-                stepSize={0.01}
-                labelStepSize={0.25}
-                onChange={this.handleChange}
-                value={round.get("guess")}
-                showTrackFill={false}
-                disabled={isResult}
-              />
-            </div>
-          </div>
-
-          {isResult ? (
-            <table className="pt-table  pt-html-table pt-html-table-bordered">
-              <thead>
-                <tr>
-                  <th>Your guess</th>
-                  <th>Actual correlation</th>
-                  <th>Score increment</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{round.get("guess") || "No guess given"}</td>
-                  <td>{round.data.task.correctAnswer}</td>
-                  <td>{round.get("score")}</td>
-                </tr>
-              </tbody>
-            </table>
-          ) : (
-            ""
-          )}
-
-          <div className="pt-form-group">
+{/*          <div className="pt-form-group">
             <button type="submit" className="pt-button pt-icon-tick pt-large">
               {isResult ? "Next" : "Submit"}
             </button>
-          </div>
-        </form>
+          </div>*/}
       </div>
     );
   }
--- app/imports/game/client/components/TaskStimulus.jsx
+++ app/imports/game/client/components/TaskStimulus.jsx
@@ -5,7 +5,25 @@ export default class TaskStimulus extends React.Component {
   render() {
     return (
       <div className="task-stimulus">
-        <img src={this.props.taskParam} className="task-image" />
+        <table className="pt-table task-table">
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td className="cooperate">Cooperate (Partner)</td>
+                    <td className="compete">Compete (Partner)</td>
+                </tr>
+                <tr>
+                    <td className="cooperate">Cooperate (You)</td>
+                    <td>5.5</td>
+                    <td>1.7</td>
+                </tr>
+                <tr>
+                    <td className="compete">Compete (You)</td>
+                    <td>7.1</td>
+                    <td>3.3</td>
+                </tr>
+            </tbody>
+        </table>
       </div>
     );
   }
