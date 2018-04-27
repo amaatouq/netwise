c1b6b2571bcd57f8a0301231b998e5864e8f0581
Abdullah <amaatouq@gmail.com>
2018-04-26 23:50:19 -0400

    Added richer behavior to the bot (updating guesses & following top k) .. However, it is not working as all the attributes of the 'game' object are undefined.

--- app/imports/game/server/bots.js
+++ app/imports/game/server/bots.js
@@ -7,18 +7,45 @@ export const bob = {
   // onStageStart(bot, game, round, stage, players) {},
 
   // Called during each stage at tick interval (~1s at the moment)
+  //TODO: the bot should have something that indicates that everyone else submitted.
+  //something like: if all.submitted? bot.submit();
   onStageTick(bot, game, round, stage, players, secondsRemaining) {
+    //get alters of the bot
+    const alterIds = bot.get("alterIds");
+    const allPlayers = _.sortBy(game.players, p =>
+      p.get("cumulativeScore")
+    ).reverse();
+    const alters = allPlayers.filter(p => alterIds.includes(p._id));
+
     if (bot.stage.submitted) {
       return;
     }
 
-    if (stage.name !== "outcome") {
+    if (stage.name === "response") {
       const guess = Math.random();
       bot.stage.set("guess", guess);
       bot.round.set("guess", guess);
+      bot.stage.submit();
+    } else if (stage.name === "interactive") {
+      //this should run when a player has changed a value, not every second
+      let currentGuess = bot.round.get("guess");
+      alters.forEach(alter => {
+        currentGuess = currentGuess + alter.round.get("guess");
+      });
+      console.log("bob's updated guess",currentGuess/alterIds.length);
+      bot.stage.set("guess", currentGuess/alterIds.length );
+      bot.round.set("guess", currentGuess/alterIds.length );
+    } else if (stage.name === "outcome") {
+      //follow top k performing players
+      //console.log("from inside bot in round outcome",game);
+  
+      //if (game.treatment.rewiring) {
+        //bot.set("alterIds", allPlayers.slice(0, game.treatment.altersCount));
+        bot.set("alterIds", allPlayers.slice(0, 2));
+        bot.stage.submit();
     }
 
-    if (secondsRemaining <= 100) {
+    if (secondsRemaining <= 50) {
       bot.stage.submit();
     }
   }
